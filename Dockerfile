3a3f03c65eadbe237e13893b69974dc9871a72d8
Mikael Johansson <mikl.johansson@gmail.com>
2016-10-22 21:23:41 +0200

    Moved scripts to /bin to avoid readonly root directory

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM alpine:latest
 
 RUN apk -U add python
 
-COPY mesos-statsd.py /
-COPY mesos-statsd.sh /
+COPY mesos-statsd.py /bin
+COPY mesos-statsd.sh /bin
 
-ENTRYPOINT ["/mesos-statsd.sh"]
+ENTRYPOINT ["mesos-statsd.sh"]
--- mesos-statsd.sh
+++ mesos-statsd.sh
@@ -2,4 +2,4 @@
 
 # Run Python script in subprocess to make sure that [defunct] processes are reaped 
 # properly by bash. Google "docker PID 1 zombie reaping problem" for more info.
-python /mesos-statsd.py $@ & wait
+python /bin/mesos-statsd.py $@ & wait
